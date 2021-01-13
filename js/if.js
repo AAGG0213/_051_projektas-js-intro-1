9b974d240b5475fa745ec350c28df0ce50459894
AAGG0213 <Aurelija.Gefeniene@gmail.com>
2021-01-13 13:12:08 +0200

    Papildyta ir baigta su if

--- js/if.js
+++ js/if.js
@@ -1,3 +1,9 @@
+console.log('========================================');
+console.log('---Kintamuju palyginimas su if ---------');
+console.log('========================================');
+
+// 2021-01-06, Pirma dalis, nuo 27 min
+
 /* loginis palyginimas
 if (salyga) {
         ka daryti, kai salyga tenkinama
@@ -31,6 +37,8 @@ console.log('-------------------------------');
 
 if (a > b) 
     console.log('jei a daugiau uz b ir nera else');
+    
+// Siuo atveju nieko nedaroma, kai salyga netenkinama
 
 console.log('-------------------------------');
 
@@ -42,7 +50,7 @@ if (a > b) {
 
 console.log('-------------------------------');
 
-const diena2 = 'tre';
+const diena2 = 'penkt';
 
 if (diena2 === 'pirm') {
     console.log('Pirmadienis');
@@ -50,11 +58,35 @@ if (diena2 === 'pirm') {
     if (diena2 === 'antr') {
         console.log('Antradienis');
         } else {
-        console.log('Kita savaites diena');
-}
+            if (diena2 === 'trec') {
+                console.log('Treciadienis');
+            } else {
+                if (diena2 === 'ketv') {
+                    console.log('Ketvirtadienis');
+                }
+                    else {
+                        console.log('Kita savaites diena');
+                    }             
+        }
+    }
+    
 }
 
-console.log('-------------------------------');
-console.log('-------------------------------');
-console.log('-------------------------------');
-console.log('-------------------------------');
+
+const diena3 = 'penk';
+
+if (diena3 === 'pirm') {console.log('Pirmadienis');
+} else if (diena3 === 'antr') {console.log('Antradienis');
+} else if (diena3 === 'trec') {console.log('Treciadienis');
+} else if (diena3 === 'ketv') {console.log('Ketvirtadienis');
+} else if (diena3 === 'penk') {console.log('Penktadienis');
+} else if (diena3 === 'sest') {console.log('Sestadienis');
+} else if (diena3 === 'sekm') {console.log('Sekmadienis');
+} else {console.log('Nera tokios dienos');}
+
+
+
+
+console.log('========================================');
+console.log('---palyginimo su if pabaiga ------------');
+console.log('========================================');
